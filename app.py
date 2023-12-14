a4c88e33a23cd36d00f4694a7b705d69f0ba5b09
Ashley Cao <52900646+yuanc0000@users.noreply.github.com>
2023-12-14 21:31:27 +0000

    web working v1

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
+# Basic Website Template
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("home.html", title="Home")
+    return render_template('home.html', title='Home')
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About Us')
 
-@app.route("/use")
-def use():
-    return render_template("use.html", title="Use App")
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=23333)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
