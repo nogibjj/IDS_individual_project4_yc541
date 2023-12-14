4b310b6c7d4e4e723911af5401715c911ea0c0d3
Ashley Cao <52900646+yuanc0000@users.noreply.github.com>
2023-12-14 22:34:17 +0000

    update

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
 # Basic Website Template
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from openai import OpenAI
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return render_template('home.html', title='Home')
 
-
 @app.route('/about')
 def about():
     return render_template('about.html', title='About Us')
+
+@app.route('/use')
+def use():
+    return render_template('use.html', title='Use App')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == "POST":
+        query = request.form['search_query']
+        client = OpenAI(
+            # Replace it with your own api key
+            api_key = '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'
+        )
+        response = client.chat.completions.create(
+        messages=[
+            {
+            "role": "user",
+            "content": query,
+            }
+        ],
+        model="gpt-3.5-turbo",
+        )
+        return render_template('search_results.html', response=response.choices[0].message.content)
+    return render_template('search.html')
 
 
 if __name__ == '__main__':
