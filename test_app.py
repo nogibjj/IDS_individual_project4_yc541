b949f03bdf6d6e313180b3af1044a798b3318110
Ashley Cao <52900646+yuanc0000@users.noreply.github.com>
2023-12-16 01:20:11 +0000

    update

--- test_app.py
+++ test_app.py
@@ -11,15 +11,11 @@ class BasicTests(unittest.TestCase):
         app.testing = True
         self.app = app.test_client()
 
-    # 测试首页是否返回状态码 200
+    # test 200
     def test_home(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
 
-    # 测试 "关于我们" 页面是否返回状态码 200
-    def test_about(self):
-        response = self.app.get('/about')
-        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
